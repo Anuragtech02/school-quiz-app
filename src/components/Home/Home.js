40c32a392b3cf93a0af635e11c91977bf3d68df7
Anuragtech02 <apal895@gmail.com>
2020-08-20 22:22:28 +0530

    Iniated UI for dashboard

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import styles from "./Home.module.css";
+import { Grid } from "@material-ui/core";
+import classNames from "classnames";
 
 export const Home = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <Grid spacing={2} container>
+          <Grid item xs={12}>
+            <div className={classNames(styles.card, styles.testsTaken)}>
+              <h1>2/4</h1>
+              <h4>Tests taken</h4>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
